a9a03f33fbc22c4f3dbb9ce4348b10e51f495347
ChristopherCautivar <ccautivar@csumb.edu>
2022-02-18 23:15:57 -0800

    fixed navbar css and sizing. HabitTracker now uses a string stored in state in conjunction with a ref connected to the input month input field  to render the habit calendar

--- src/components/HabitTracker.js
+++ src/components/HabitTracker.js
@@ -1,21 +1,18 @@
-import React, { useState, useRef, useEffect} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import HabitCalendar from './HabitCalendar';
 import './HabitTracker.css'
 
 export default function HabitTracker() {
   const habitMonth = useRef()
   const habitName = useRef()
-  // const [monthTouched, setMTouched] = useState(0)
-  const [calendar, setCalendar] = useState(<div>Choose a month and a habit to start!</div>)
+  // useState is responsible for triggering rerenders, otherwise habitMonth's useRef would be enough
+  // to access and set the variable
+  const [calendar, setCalendar] = useState("")
   const [habits, setHabits] = useState(["this habit"])
 
-  // a habit "object" will have a name, a calendar, and an id
-  // a calendar "object" will have a month-year name, and days
-  // a day "object" will have a number and the state of the habit
-
-  // useEffect(() => {
-  //   console.log("ya")
-  // }, [habitName.current.value])
+  // a habit component will have a name, a calendar, and an id
+  // a calendar component will have a month-year name, and days
+  // a day component will have a number and the state of the habit
 
   function handleAddHabit(e){
     const name = habitName.current.value
@@ -26,8 +23,8 @@ export default function HabitTracker() {
 
   return (
       <div className='Component'>
-        <input ref={habitMonth} type="month" onChange={
-          () => setCalendar(<HabitCalendar monthSelected={habitMonth.current.value}/>)}/>
+        <input ref={habitMonth} type="month" value={calendar} onChange={
+          ()=>setCalendar(habitMonth.current.value)}/>
         {" "}
         <select>
           {habits.map(habit=><option>{habit}</option>)}
@@ -35,7 +32,7 @@ export default function HabitTracker() {
         {" "}
         <input ref={habitName} type="text"/>
         <button onClick={handleAddHabit}>Add Habit</button>
-        {calendar}
+        {calendar ? <HabitCalendar monthSelected={calendar}/> : <div>Choose a month and a habit to start!</div>}
       </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav className='Component'>
+    <nav>
       <span>
         <Link to="/">Home</Link>{" "}|{" "}
         <Link to="/todolist">Todo List</Link>{" "}|{" "}
