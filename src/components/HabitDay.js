a4b5f04ec627aac976e9e403e86eebb0f3e6a544
ChristopherCautivar <ccautivar@csumb.edu>
2022-03-23 01:06:49 -0700

    Habit tracker reached MVP status. Added persistence and altered logic to correctly utilize state, component, and data binding conventions while preserving the look and feel of the non-functional prototype in previous commits. See diff for more information. Next some light refactoring to increase readability, and maybe some labeling of the calendar or resizing of the input elements before merging to prod.

--- src/components/HabitDay.js
+++ src/components/HabitDay.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './HabitDay.css'
 
-export default function HabitDay({ day, tracked, completeHabit }) {
+export default function HabitDay({ habitDay, completeHabit }) {
+  // TODO: refactor naming, its getting confusing
+  function handleDayClick() {
+    completeHabit(habitDay.id)
+  }
   return (
     <td>
-        <button disabled={!Boolean(day)} onClick={completeHabit} className={
-          tracked ? "tracked" : "untracked" }>{day}</button>
+        <button disabled={!Boolean(habitDay.day)} onClick={handleDayClick} className={
+          habitDay.tracked ? "tracked" : "untracked" }>{habitDay.day}</button>
     </td>
   );
 }
